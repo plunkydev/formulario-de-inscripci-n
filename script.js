67e69647f81f5cf8cf40c25590a29d6a6ba91f5e
plunkydev <137208896+plunkydev@users.noreply.github.com.>
2024-01-29 17:50:36 -0400

    Creacion funcion para comparar contrasenas si coinciden

--- script.js
+++ script.js
@@ -1,2 +1,15 @@
-const password = document.querySelector("#password");
-const passwordConfir = document.querySelector("#password-confir");
+function comprobarContrasenas() {
+    var password1 = document.getElementById("password").value;
+    var password2 = document.getElementById("password-confir").value;
+    if (password1 === password2) {
+        document.getElementById("password-confir").classList.add("borde-focus");
+        document.getElementById("password").classList.add("borde-focus");
+        document.getElementById('match').style.color = "#596D48"
+        document.getElementById("match").innerHTML = "Las contraseñas coinciden";
+    } else {
+        document.getElementById("password-confir").classList.remove("borde-focus");
+        document.getElementById("password").classList.remove("borde-focus");
+        document.getElementById('match').style.color = "#de5570"
+        document.getElementById("match").innerHTML = "*Las contraseñas no coinciden";
+    }
+}
